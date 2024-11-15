4da11976607ea09b12dba71a151342bc3f5353dd
Ashish Sharma <ashish.arunkumar.sharma@emory.edu>
2024-11-15 10:23:30 -0500

    Libraries Added

--- Seurat_to_h5ad_for_Scimilarity.R
+++ Seurat_to_h5ad_for_Scimilarity.R
@@ -1,3 +1,5 @@
+library(Seurat)
+library(SeuratDisk)
 CITN <- readRDS("/Users/aashar4/Documents/2022-2024/ManuscriptCITN12/scRNAseq/CITN_scRNAseq_from_Adam/PBMC_seurat_object_with_module_scores.RDS")
 CITN@assays$RNA$scale.data <- NULL
 SaveH5Seurat(CITN, overwrite = TRUE)
